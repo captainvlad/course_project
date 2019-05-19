70717612f762135057f1ebc372554b6e1ae768f9
captainvlad <zadorozhnyy@ucu.edu.ua>
2019-05-19 17:14:41 +0300

    Commit

---
+++ Etap_5/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Course Work',
+      version='1.0',
+      description='Course work 1 course',
+      author='Zadorozhnyy Vladyslav',
+      author_email='zadorozhnyy@ucu.edu.ua',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['final_version'])
